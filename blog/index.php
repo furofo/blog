e8d6d2f959b335116d390d503b9a66dd53e44916
Christian Malpass <chriscellphonerepair@gmail.com>
2019-04-24 22:36:23 -0400

    connecting database to index.php

--- blog/index.php
+++ blog/index.php
@@ -1,4 +1,73 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+<?php
+// Work out the path to the database, so SQLite/PDO can connect
+$root = __DIR__;
+$database = $root . '/data/data.sqlite';
+$dsn = 'sqlite:' . $database;
+// Connect to the database, run a query, handle errors
+$pdo = new PDO($dsn);
+$stmt = $pdo->query(
+    'SELECT
+        title, created_at, body
+    FROM
+        post
+    ORDER BY
+        created_at DESC'
+);
+if ($stmt === false)
+{
+    throw new Exception('There was a problem running this query');
+}
+?>
+
+
 <!DOCTYPE html>
+
 <html>
     <head>
         <title>A blog application</title>
